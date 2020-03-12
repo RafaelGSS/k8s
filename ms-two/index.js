c0f28ebfacded00c52813225735209d8fbcfbc9d
RafaelGSS <rafael.nunu@hotmail.com>
2020-03-11 22:50:55 -0300

    update(ms-two): add env and configmap usage

--- ms-two/index.js
+++ ms-two/index.js
@@ -2,11 +2,16 @@ const Fastify = require('fastify')
 
 const server = Fastify({ logger: 'info' })
 server.get('/', (_request, reply) => {
-  reply.send({ hello: 'guys', timestamp: date.now() })
+  reply.send({
+    hello: 'guys',
+    timestamp: Date.now(),
+    dbPass: process.env.DB_PASSWORD,
+    gameProperties: process.env['game.properties']
+  })
 })
 
 server.get('/throw', (_request, reply) => {
-  throw new Error('Shutup!')
+  throw new Error('Shutup!!')
   reply.send({ hello: 'guys', timestamp: Date.now() })
 })
 
