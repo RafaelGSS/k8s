eea0a39a370fb78c8cecfa42fc8ce3c037b78280
RafaelGSS <rafael.nunu@hotmail.com>
2020-03-11 22:50:24 -0300

    update(ms-one): add NODE_NAME

--- ms-one/index.js
+++ ms-one/index.js
@@ -2,7 +2,7 @@ const Fastify = require('fastify')
 
 const server = Fastify({ logger: 'info' })
 server.get('/', (_request, reply) => {
-  reply.send({ hello: 'world', timestamp: Date.now() })
+  reply.send({ hello: 'world', timestamp: Date.now(), name: process.env.NODE_NAME })
 })
 
 server.listen(3000, '0.0.0.0', err => {
